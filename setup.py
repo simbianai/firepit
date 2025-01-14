3db464d6c778523fa561096be70627bf8839fe74
vishwas <vishwas@simbian.ai>
2025-01-14 14:56:37 +0530

    Bump version: 2.3.35 → 2.3.38

--- setup.py
+++ setup.py
@@ -63,6 +63,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/opencybersecurityalliance/firepit",
-    version='2.3.35',
+    version='2.3.38',
     zip_safe=False,
 )
